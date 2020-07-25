901fa705960f7089b9bfb0d7f50a29666d5cb8b8
aaronlelevier <aaron.lelevier@gmail.com>
2020-07-25 08:23:20 -0700

    raaw_urls:urls/0 - same interface as 'chromag:urls/0' to get a list of all urls

--- apps/web/src/raaw_urls.erl
+++ apps/web/src/raaw_urls.erl
@@ -8,8 +8,11 @@
 -module(raaw_urls).
 -author("Aaron Lelevier").
 -vsn(1.0).
--export([madonna_v2_frame_kit/0, madonna_fox_factory_build/0,
+-export([urls/0, madonna_v2_frame_kit/0, madonna_fox_factory_build/0,
   madonna_xtr_build_url/0]).
+
+urls() ->
+  [madonna_v2_frame_kit(), madonna_fox_factory_build(), madonna_xtr_build_url()].
 
 madonna_v2_frame_kit() ->
   "https://raawmtb.com/collections/frames-bikes/products/madonna-v2-frame-kit".

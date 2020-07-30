52a9218544721def45da37c6f9b5b928e7434931
aaronlelevier <aaron.lelevier@gmail.com>
2020-07-30 08:01:28 -0700

    web_worker is sending params to web_reporter and web_reporter then logs them

--- apps/web/src/web_worker.erl
+++ apps/web/src/web_worker.erl
@@ -3,31 +3,30 @@
 %%% @doc
 %%% @end
 %%%-------------------------------------------------------------------
--module(web_reporter).
+-module(web_worker).
 -behaviour(gen_server).
 
 -include_lib("dta/include/macros.hrl").
 
 %% API
--export([start_link/0, receive_report/1]).
+-export([start_link/0, send_report/2]).
 
 %% gen_server
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
   code_change/3]).
 
-%% Macros
--define(SERVER, ?MODULE).
 
 %%%===================================================================
 %%% API
 %%%===================================================================
 
 start_link() ->
-  gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+  gen_server:start_link(?MODULE, [], []).
 
-receive_report({Url, BikeMod}) ->
-  ?LOG({self(), receive_report, start}),
-  gen_server:call(?SERVER, {receive_report, {Url, BikeMod}}).
+send_report(Pid, {Url, BikeMod}) ->
+  ?LOG({self(), send_report, start}),
+  % TODO: change to 'cast' ?
+  gen_server:call(Pid, {send_report, {Url, BikeMod}}).
 
 %%%===================================================================
 %%% Spawning and gen_server implementation
@@ -36,8 +35,9 @@ receive_report({Url, BikeMod}) ->
 init([]) ->
   {ok, #{}}.
 
-handle_call({receive_report, {Url, BikeMod}}, _From, State) ->
-  ?LOG({self(), report_received, Url, BikeMod}),
+handle_call({send_report, {Url, BikeMod}}, _From, State) ->
+  ?LOG({self(), report_sent, Url, BikeMod}),
+  ok = web_reporter:receive_report({Url, BikeMod}),
   {reply, ok, State}.
 
 handle_cast(_Request, State) ->

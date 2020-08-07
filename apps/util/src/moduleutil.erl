57158aaabe6cae3b7d62362ab11e78b11dcd3a15
aaronlelevier <aaron.lelevier@gmail.com>
2020-08-07 09:15:06 -0700

    helper for outputting all functions to export - this helps if first using '-compile(export_all)' so functions don't have to be added 1 by 1

---
+++ apps/util/src/moduleutil.erl
@@ -0,0 +1,28 @@
+%%%-------------------------------------------------------------------
+%%% @author Aaron Lelevier
+%%% @doc Erlang Module helpers
+%%%
+%%% @end
+%%% Created : 07. Aug 2020 8:57 AM
+%%%-------------------------------------------------------------------
+-module(moduleutil).
+-author("Aaron Lelevier").
+-vsn(1.0).
+-export([show_exports/1]).
+
+%% @doc helper for outputting all functions to export
+%% example call:
+%% $ moduleutil:show_exports(foo).
+%% $ -export([foo/1])
+%% $ ok.
+-spec show_exports(atom()) -> ok.
+show_exports(Mod) ->
+  Exports = [
+    lists:flatten(io_lib:format("~p/~p", [X, Y])) ||
+    {X, Y} <- Mod:module_info(exports),
+    X /= module_info],
+  io:format(
+    "-export([~s]).~n", [string:join(Exports, ", ")]
+  ).
+
+

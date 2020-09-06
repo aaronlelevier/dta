c1b553b5e15027d3cafd087f8ca8313786e2c511
aaronlelevier <aaron.lelevier@gmail.com>
2020-09-06 08:23:08 -0700

    [web_url] fixup public/private api differentiation

--- apps/web/src/web_url.erl
+++ apps/web/src/web_url.erl
@@ -10,29 +10,31 @@
 -vsn(1.0).
 -export([brand/1, bike/1, bike_mod/1]).
 
+%%%===================================================================
+%%% API
+%%%===================================================================
 
-%% Public
 -spec brand(web_request:url()) -> string().
 brand(Url) ->
   regex_url(Url, <<"https://([a-zA-z0-9]+)">>).
-
 
 -spec bike(web_request:url()) -> string().
 bike(Url) ->
   regex_url(Url, <<"/([a-zA-z0-9\-]+?)$">>).
 
+%% @doc Returns the bike_mod based on the Url
+-spec bike_mod(Url :: web_request:url()) -> atom().
+bike_mod(Url) ->
+  Brand = brand(Url),
+  BrandMap = #{"chromagbikes" => chromag, "raawmtb" => raaw},
+  maps:get(Brand, BrandMap).
 
-%% Private
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
 -spec regex_url(string(), binary()) -> string().
 regex_url(Url, Re) ->
   Bin = list_to_binary(Url),
   {match,[_, H2 | _]} = re:run(Bin, Re, [{capture, all, list}]),
   H2.
-
-
-%% @doc Returns the bike_mod based on the Url
--spec bike_mod(Url :: web_request:url()) -> atom().
-bike_mod(Url) ->
-  Brand = web_url:brand(Url),
-  BrandMap = #{"chromagbikes" => chromag, "raawmtb" => raaw},
-  maps:get(Brand, BrandMap).
